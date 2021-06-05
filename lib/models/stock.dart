1661b6a3863947742934dea692e3cc2bcafd1d28
ayushsawnani <81490699+ayushsawnani@users.noreply.github.com>
2021-06-04 23:07:04 -0400

    added data for each buy/sell page, updated rate in stocklist

--- lib/models/stock.dart
+++ lib/models/stock.dart
@@ -2,12 +2,14 @@ class Stock {
   final String? symbol;
   final String? company;
   final double? price;
+  final double? rate;
   bool? isPressed;
-  Stock({this.symbol, this.company, this.price, this.isPressed});
+  Stock({this.symbol, this.company, this.price, this.isPressed, this.rate});
 
   String? get companyName => company!;
   String? get symbolName => symbol!;
   double? get stockPrice => price!;
+  double? get ratePercent => rate!;
 
   void setPressed(bool? pressed2) => isPressed = pressed2;
 }
--- lib/pages/buyandsellpage.dart
+++ lib/pages/buyandsellpage.dart
@@ -36,43 +36,80 @@ class _buysellpage extends State<BuySellPage> {
                     padding: EdgeInsets.all(10.0),
                     width: MediaQuery.of(context).size.width + 100,
                     color: Colors.black,
-                    child: SafeArea(
-                      child: Row(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
-                          Padding(
-                              padding: EdgeInsets.all(0.0),
-                              child: TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => TradePage()));
-                                },
-                                style: TextButton.styleFrom(
-                                  backgroundColor: Colors.grey[600],
-                                  shadowColor: Colors.white,
-                                  primary: Colors.red,
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 10.0, vertical: 10.0),
-                                  minimumSize: Size(5, 2.5),
-                                ),
-                                child: Text("back",
-                                    style: TextStyle(fontSize: 10.0)),
-                              )),
-                          Container(
-                              padding: EdgeInsets.all(30.0),
-                              width: MediaQuery.of(context).size.width,
-                              color: Colors.black,
-                              child: (SafeArea(
-                                  child: Text(
-                                      "${stocks!.trueStock()!.companyName}",
-                                      style: TextStyle(
-                                          color: checkColor(stock!.stockPrice),
-                                          fontSize: 36,
-                                          fontWeight: FontWeight.bold)))))
-                        ],
-                      ),
-                    )),
+                          SafeArea(
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                    padding: EdgeInsets.all(0.0),
+                                    child: TextButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    TradePage()));
+                                      },
+                                      style: TextButton.styleFrom(
+                                        backgroundColor: Colors.grey[600],
+                                        shadowColor: Colors.white,
+                                        primary: Colors.white,
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 10.0, vertical: 10.0),
+                                        minimumSize: Size(5, 2.5),
+                                      ),
+                                      child: Text("back",
+                                          style: TextStyle(fontSize: 10.0)),
+                                    )),
+                                Container(
+                                    padding: EdgeInsets.all(30.0),
+                                    width: MediaQuery.of(context).size.width,
+                                    color: Colors.black,
+                                    child: (SafeArea(
+                                        child: Text(
+                                            "${stocks!.trueStock()!.companyName}",
+                                            style: TextStyle(
+                                                color: checkColor(
+                                                    stock!.stockPrice),
+                                                fontSize: 36,
+                                                fontWeight: FontWeight.bold)))))
+                              ],
+                            ),
+                          ),
+                          SafeArea(
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                Text("${stocks!.trueStock()!.symbol}",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 36,
+                                        fontWeight: FontWeight.bold)),
+                                Text("\$${stocks!.trueStock()!.price}",
+                                    style: TextStyle(
+                                        color: checkColor(stock.stockPrice),
+                                        fontSize: 36,
+                                        fontWeight: FontWeight.bold)),
+                                SizedBox(
+                                  child: Container(
+                                      width: 150,
+                                      height: 50,
+                                      child: Text(
+                                          "${stocks!.trueStock()!.rate}%",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 24)),
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        color: checkColor(stock.rate),
+                                      )),
+                                )
+                              ]))
+                        ])),
               ])))
     ]));
   }
