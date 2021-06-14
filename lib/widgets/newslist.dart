26c6dce2c44f6fadc075e0630241e571404ecf3f
ayushsawnani <81490699+ayushsawnani@users.noreply.github.com>
2021-06-14 13:53:49 -0400

    updated images

--- lib/widgets/newslist.dart
+++ lib/widgets/newslist.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:stock_simulator/models/news_models/news_article.dart';
 import 'package:stock_simulator/models/news_models/view_models/newsarticle_listview.dart';
 
 class NewsList extends StatelessWidget {
-  String _displayMedia(String? media) {
+  dynamic _displayMedia(String? media) {
     if (media == null) {
-      return '/assets/asfalt-light.png';
+      return Image.asset('/assets/asfalt-light.png');
     } else
-      return media;
+      return Image.network(media);
   }
 
   @override
   Widget build(BuildContext context) {
     final vm = Provider.of<NewsArticleListViewModel>(context);
-    //TODO: implement build
     return ListView.builder(
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
       itemCount: vm.articles.length,
       itemBuilder: (context, index) {
         return ListTile(
-          leading: FadeInImage.assetNetwork(
-              placeholder: 'N/A',
-              image: _displayMedia(vm.articles[index].imageURL)),
+          leading: _displayMedia(vm.articles[index].imageURL),
           title: Text(vm.articles[index].title,
               style: TextStyle(color: Colors.white)),
         );
