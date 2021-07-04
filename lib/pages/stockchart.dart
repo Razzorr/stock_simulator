2255b3bc9de751defcd9b1c85b808ef3db9c0a9e
Razzorr <kviraj223@gmail.com>
2021-07-04 03:03:33 -0400

    Stockchart finished testing

--- lib/pages/stockchart.dart
+++ lib/pages/stockchart.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:candlesticks/candlesticks.dart';
+import 'package:stock_simulator/models/stockchart_models/services/repository.dart';
 
 class StockChart extends StatefulWidget {
   final String? symbol;
 
   StockChart(this.symbol);
 
-  _stockchart createState() => _stockchart(symbol);
+  _stockchart createState() => _stockchart(symbol!);
 }
 
 class _stockchart extends State<StockChart> {
-  final String? symbol;
+  List<Candle> candles = [];
+
+  final String symbol;
 
   _stockchart(this.symbol);
 
   @override
+  void initState() {
+    fetchCandles(symbol: this.symbol, interval: "1h").then((value) {
+      setState(() {
+        candles = value;
+      });
+      print(value[0].date.day);
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("candleSticks"),
+      ),
+      body: Center(
+        child: AspectRatio(
+          aspectRatio: 1.2,
+          child: Candlesticks(
+            candles: candles,
+          ),
+        ),
+      ),
+    );
   }
 }
